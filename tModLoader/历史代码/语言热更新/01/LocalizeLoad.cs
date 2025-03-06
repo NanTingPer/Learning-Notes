09166d58ea2d827f5ec3f9ce644a6ff50236db77
Nan_Ting <a3380991070@163.com>
2025-03-06 23:39:49 +0800

    语言热更新 解决{$}的问题

---
+++ tModLoader/历史代码/语言热更新/01/LocalizeLoad.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader.Config;
+
+namespace ThoriumModzhcn.Systems
+{
+    public static class LocalizeLoad
+    {
+        public static List<string> HKHJson { get; } =
+         [
+            "zh-hk",
+            "zh-hk_Mods.CalamityBardHealer.hjson",
+            "zh-hk_Mods.RedemptionBardHealer.hjson",
+            "zh-hk_Mods.SOTSBardHealer.hjson",
+            "zh-hk_Mods.SpiritBardHealer.hjson",
+            "zh-hk_Mods.SpookyBardHealer.hjson",
+            "zh-hk_Mods.TerrariumHacks.hjson",
+            "zh-hk_Mods.ThoriumRework.hjson",
+            "zh-hk_Mods.UnlimitedBardEnergy.hjson"
+        ];
+        public static List<string> TWHJson { get; } =
+         [
+            "zh-tw",
+            "zh-tw_Mods.CalamityBardHealer.hjson",
+            "zh-tw_Mods.RedemptionBardHealer.hjson",
+            "zh-tw_Mods.SOTSBardHealer.hjson",
+            "zh-tw_Mods.SpiritBardHealer.hjson",
+            "zh-tw_Mods.SpookyBardHealer.hjson",
+            "zh-tw_Mods.TerrariumHacks.hjson",
+            "zh-tw_Mods.ThoriumRework.hjson",
+            "zh-tw_Mods.UnlimitedBardEnergy.hjson"
+        ];
+
+        public static List<string> ZHHJson { get; } = [];
+        static LocalizeLoad()
+        {
+            Load(HKHJson, LocalizeUtil.Language.HongKong);
+            Load(TWHJson, LocalizeUtil.Language.TaiWan);
+            Load(ZHHJson, LocalizeUtil.Language.Chinese);
+        }
+
+        private static void Load(List<string> paths, LocalizeUtil.Language language)
+        {
+            foreach (var hjson in paths) {
+                LocalizeUtil.LoadLocalizedKey(hjson, language, GetStartChars(hjson));
+            }
+        }
+
+        private static string GetStartChars(string fileName)
+        {
+            var splitString = fileName.Split("_");
+            if (splitString.Length == 1) return "";
+            if (splitString[0] == "IL") return "";
+            return splitString[1].Replace(".hjson", "");
+        }
+    }
+
+    public class 语言切换 : ModConfig
+    {
+        public LocalizeUtil.Language 语言;
+
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        public override void OnChanged()
+        {
+            LocalizeUtil.CutLanguage(语言);
+            base.OnChanged();
+        }
+    }
+}
---
+++ tModLoader/历史代码/语言热更新/02解决{$}的问题/ForceLocalizeSystem.cs
@@ -0,0 +1,109 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Core;
+
+namespace ThoriumModzhcn.Systems
+{
+
+    //private class TranslatorLoad : ForceLocalizeSystem<TranslatorLoad, StructureHelper> { }
+    public class UnLoad : ModSystem
+    {
+        /// <summary>
+        /// Language.GetTextValue，使用Key获取值
+        /// </summary>
+        public static MethodInfo GetTextValue { get; } = typeof(Language).GetMethods().FirstOrDefault(met => met.Name == "GetTextValue" && met.GetParameters().Length == 1); //Language.GetTextValue;
+        /// <summary>
+        /// 全部类型的全类名和这个类型的引用
+        /// </summary>
+        public static List<Dictionary<string, Type>> List { get; } = [];
+        /// <summary>
+        /// 全部钩子
+        /// </summary>
+        public static List<ILHook> ILHooks { get; } = [];
+        public override void PostSetupContent()
+        {
+            foreach (var item in List) {
+                item.Clear();
+            }
+            base.PostSetupContent();
+        }
+
+        public override void Unload()
+        {
+            ILHooks.Clear();
+            base.Unload();
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TSale">目标模组</typeparam>
+    public class ForceLocalizeSystem<TSale, 占位符>
+    {
+        public static readonly Dictionary<string, Type> Types = [];
+
+        /// <summary>
+        /// 只会初始化一次，用来载入模组的全部类型
+        /// </summary>
+        static ForceLocalizeSystem()
+        {
+            if (!ModLoader.TryGetMod(typeof(TSale).Name, out Mod TarGet))
+                return;
+            foreach (var item in AssemblyManager.GetLoadableTypes(TarGet.Code)) {
+                Types.Add(item.FullName, item);
+            }
+            UnLoad.List.Add(Types);
+        }
+        /// <summary>
+        /// 对给定全名的Type的给定方法进行字符串替换
+        /// </summary>
+        /// <param name="TypeName">全类名</param>
+        /// <param name="MethodName">方法名</param>
+        /// <param name="Value">值</param>
+        public static void LocalizeByTypeFullName(string TypeName, string MethodName, Dictionary<string, string> Value)
+        {
+            //value结构 :
+            //      Key => 本地化键
+            //      Value => 英文值
+            if (!ModLoader.TryGetMod(typeof(TSale).Name, out Mod TarGet)) return;
+            if (!Types.TryGetValue(TypeName, out Type type)) return;
+            MethodInfo Method = type.GetMethod(MethodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            if (Method is null) return;
+
+            ILHook iLHook =
+            new ILHook(Method, ilc => {
+                ILHookMethod(ilc, Value);
+            });
+            iLHook.Apply();
+            UnLoad.ILHooks.Add(iLHook);
+        }
+
+        private static void ILHookMethod(ILContext ilc, Dictionary<string, string> Value)
+        {
+            ILCursor ILCursor = new ILCursor(ilc);
+            var r = ilc.Body.Instructions;
+            while (ILCursor.Next != null) {
+                if (ILCursor.TryGotoNext(MoveType.After, il => il.MatchLdstr(out _))) {
+                    var oldString = ILCursor.Previous.Operand.ToString();
+                    foreach (var Key in Value.Keys) {
+                        if (Value[Key] == oldString) {
+                            ILCursor.Index--;
+                            ILCursor.Remove();
+                            ILCursor.Emit(OpCodes.Ldstr, Key);
+                            ILCursor.Emit(OpCodes.Call, UnLoad.GetTextValue);
+                            ILCursor.Index++;
+                            break;
+                        }
+                    }
+                } else break;
+            }
+        }
+    }
+}

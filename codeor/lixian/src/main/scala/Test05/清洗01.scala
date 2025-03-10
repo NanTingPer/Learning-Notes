bcafce53d69f2bee1ab52201ae325bff8db2bcc1
Nan_Ting <a3380991070@163.com>
2025-03-10 14:06:38 +0800

    创建ASP.NET8项目

--- codeor/lixian/src/main/scala/Test05/清洗01.scala
+++ codeor/lixian/src/main/scala/Test05/清洗01.scala
@@ -17,10 +17,25 @@ object 清洗01 {
         val odstable = "ods.user_info"
         val dwdtable = "dwd.dim_user_info"
 
+        //导入数据
+        spark.sql(s"select * from ${odstable} limit 10")
+            .withColumn("dim_insert_user", lit("user1"))
+            .withColumn("dim_insert_time", date_format(current_timestamp(),"yyyy-MM-dd HH:mm:ss"))
+            .withColumn("dim_modify_user", lit("user1"))
+            .withColumn("dim_modify_time", date_format(current_timestamp(),"yyyy-MM-dd HH:mm:ss"))
+            .withColumn("etl_date", lit("20250303"))
+            .write
+            .mode(SaveMode.Overwrite)
+            .partitionBy("etl_date")
+            .format("hive")
+            .saveAsTable(dwdtable)
+
+
+
         var odsdata = spark.sql(s"select *, coalesce(operate_time, create_time) as temp from ${odstable} where etl_date='20250305'")
             .drop("operate_time")
             .withColumnRenamed("temp", "operate_time")
-        val maxtime = spark.sql(s"select max(etl_date) from ${dwdtable}")
+        val maxtime = spark.sql(s"select max(etl_date) from ${dwdtable}").first()(0)
         val dwddata = spark.sql(s"select * from ${dwdtable} where etl_date='${maxtime}'")
         val allcol = dwddata.columns.map(col)
 
---
+++ codeor/lixian/src/main/scala/Test06/Sum41.scala
@@ -0,0 +1,65 @@
+package Test06
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types.DataTypes
+
+object Sum41 {
+    def main(args: Array[String]): Unit = {
+        System.setProperty("HADOOP_USER_NAME","root")
+        val spark = SparkSession.builder().appName("hive").master("local[*]").enableHiveSupport().getOrCreate()
+        //根据dwd层表统计在两天内连续下单并且下单金额保持增长的用户
+        //userid            int
+        //username      text
+        //day                       text
+        //totalconsumption double      訂單總金額
+        //totalorder                                訂單總數
+        val hiveData = "dwd.fact_order_info"
+        val hive = spark.sql("select * from dwd.fact_order_info")
+
+        //数据初步处理
+        val aggdata = hive
+            .withColumn("create_time", date_sub(col("create_time"), 0))
+            .groupBy("user_id", "create_time")
+            .agg(sum(col("final_total_amount")) as "totalconsumption", count("*") as "totalorder")
+
+        val win1 = Window.partitionBy("user_id").orderBy("create_time")
+        //不排序就是每一行都分配同一個最終結果，如果排序了那麼就是當前行分配的結果爲之前行到當前行的和
+        val win2 = Window.partitionBy("user_id", "subData")
+        val twoDay = aggdata
+            .withColumn("tempcol", row_number().over(win1))
+            .withColumn("subData", date_sub(col("create_time"), col("tempcol")))
+            .withColumn("tempsum", count("*").over(win2))
+            .where(col("tempsum") >= 2)
+            .drop("tempcol", "subData")
+//            .show
+        val win3 = Window.partitionBy("user_id").orderBy("create_time")
+
+        //过滤窗口(过滤多个两天的)
+        val win4 = Window.partitionBy("user_id")
+        twoDay
+            //lead取出給定偏移的指定列數據，是一個窗口函數
+            //lag是上一行，lead是下一行
+            .withColumn("downTotalconSumPtion", lead(col("totalconsumption"),1).over(win3))
+            //
+            .withColumn("downTotalorder", lead(col("totalorder"),1).over(win3))
+            .withColumn("downCreate_time", lead(col("create_time"),1).over(win3))
+            .where(col("downTotalorder").cast(DataTypes.StringType) =!= lit("null") and (col("totalconsumption") - col("downTotalconSumPtion") < 0))
+            .orderBy("user_id")
+            //多个连续两天处理，将前一天的消费与当前日的消费相减，取最大
+            .select("user_id", "create_time","downCreate_time", "totalconsumption", "downTotalconSumPtion", "totalorder", "downTotalorder")
+            .withColumn("subTotalconsumption", col("downTotalconSumPtion") - col("totalconsumption"))
+            .withColumn("maxtol", max("subTotalconsumption").over(win4)) //用于后面取最大差值的那条数据(增长最高)
+            .where(col("subTotalconsumption") === col("maxtol"))                //取最大差值的数据(增长最高)
+            .drop("maxtol", "subTotalconsumption")//删除计算列，保留数据列
+            .withColumn("totalconsumption", col("totalconsumption") + col("downTotalconSumPtion"))//总消费金额
+            .drop("downTotalconSumPtion")
+            .withColumn("totalorder", col("totalorder") + col("downTotalorder"))//消费单数
+            .drop("downTotalorder")
+            .withColumn("day", concat(col("create_time"), lit("_"),col("downCreate_time"))) //day
+            .drop("create_time", "downCreate_time")
+            .show()
+
+    }
+}

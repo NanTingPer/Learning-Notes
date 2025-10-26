bec1b56d9a0889f771e3f10668ffc0b990c90c9a
Nan_Ting <a3380991070@163.com>
2025-10-26 15:43:49 +0800

    TerrariaServerSystem: Create Web Project

---
+++ codeor/Cshps/ServerSystem/TerrariaServerWeb/src/Models/TerrariaServerInfoDto.ts
@@ -0,0 +1,10 @@
+﻿class TerrariaServerInfoDto 
+{
+    public Id : number = 1;
+    public Name : string = "";
+    public WorldName : string = "";
+    public Port : number = 7777;
+    public Passwd : string = "";
+}
+
+export default TerrariaServerInfoDto;
---
+++ codeor/Cshps/ServerSystem/TerrariaServerWeb/src/components/RootView.vue
@@ -0,0 +1,20 @@
+﻿<script lang="ts">
+    import TerrariaServerInfoDto from '../Models/TerrariaServerInfoDto';
+    export default() { 
+        methods: {
+            getServerList() {
+                return 1;
+            };
+        },
+    }
+</script>
+
+<template>
+<div>
+    <h1> {{ getServerList() }} </h1>
+</div>
+</template>
+
+<style>
+
+</style>

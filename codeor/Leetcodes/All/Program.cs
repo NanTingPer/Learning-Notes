2fbfef7dbfc7ba1dabca26e2d7ab9582be710113
Nan_Ting <a3380991070@163.com>
2025-11-17 09:04:27 +0800

    力扣: S1437是否所有1都至少相隔K个元素

--- codeor/Leetcodes/All/Program.cs
+++ codeor/Leetcodes/All/Program.cs
@@ -6,3 +6,4 @@ const string str = "0001";
 //Console.WriteLine(str);
 //Console.WriteLine( new S3234统计1显著字符串的数量().NumberOfSubstrings(str));
 new S1513仅含的子串数().NumSub3("0110111");
+new S1437是否所有1都至少相隔K个元素().KLengthApart([0, 1, 0, 0, 1, 0, 0, 1], 2);
---
+++ codeor/Leetcodes/All/S1437是否所有1都至少相隔K个元素.cs
@@ -0,0 +1,32 @@
+﻿namespace All;
+
+public class S1437是否所有1都至少相隔K个元素
+{
+    public bool KLengthApart(int[] nums, int k)
+    {
+        int splitZero = 0;
+        bool isInit = false;
+        for (int i = 0; i < nums.Length; i++) {
+            //遇到第一个零才开始计算，不然 001001 k=2 其实是要返回true的
+            if(nums[i] != 1 && isInit == false) {
+                continue;
+            }
+
+            //第一次的init会让nums[i] == 1 这样就直接返回false了
+            if(isInit == true) {
+                if (nums[i] == 0) {
+                    splitZero += 1;
+                } else {
+                    if (splitZero < k) {
+                        return false;
+                    }
+                    splitZero = 0;
+                }
+            }
+            
+            //跳过第一个1
+            isInit = true;
+        }
+        return true;
+    }
+}

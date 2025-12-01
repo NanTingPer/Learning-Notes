2ad559d265291c32da9b8d758b0e3f1269d1b505
Nan_Ting <a3380991070@163.com>
2025-12-01 08:21:47 +0800

    Leetcode: S28 S757

---
+++ codeor/Leetcodes/All/S28找出字符串中第一个匹配项的下标.cs
@@ -0,0 +1,22 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace All;
+
+internal class S28找出字符串中第一个匹配项的下标
+{
+    public int StrStr(string haystack, string needle)
+    {
+        int jump = needle.Length;
+        for (int i = 0; i < haystack.Length; i += jump) {
+            if (i + jump > haystack.Length) {
+                continue;
+            }
+            if (needle == haystack[i..(i + jump)]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ codeor/Leetcodes/All/S757设置交集大小至少为2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace All;
+
+/// <summary>
+/// 算出一个集合，这个集合至少包含每个子集中的2个数
+/// </summary>
+internal class S757设置交集大小至少为2
+{
+    public int IntersectionSizeTwo(int[][] intervals)
+    {
+
+        // 最终的交集
+        List<List<int>> list = new List<List<int>>();
+        for (int i = 0; i < intervals.Length; i++) {
+            list.Add(Enumerable.Range(intervals[i][0], intervals[i][1]).ToList());
+        }
+        List<int> result = new List<int>();
+        for (var i = 0; i < list.Count - 1; i++) {
+            var oneIntersect = list[i].Intersect(list[i + 1]);
+            var oneIntersectSort = oneIntersect.ToList();
+            oneIntersectSort.Sort();
+            if (oneIntersect.Count() >= 2) {
+                result.AddRange(oneIntersect.TakeLast(2));
+            } else {
+                list[i].Sort();
+                result.AddRange(list[i].TakeLast(2));
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", result));
+        
+        return 0;
+    }
+}
